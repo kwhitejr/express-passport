48dc6031ca8bc3d8bb7ebee5ea5ab18e32b28ac0
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-01 20:28:34 -1000

    first passport authentication added

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+// Modules
 var express = require('express');
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;  // Want to use Basic Authentication Strategy
 
-var app = express();
-
+// Constants
 var PORT = 3000;
+
+// Other
+var app = express();
+var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
+
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+    // Example authentication strategy using
+    if ( !(username === user.username && password === user.password) ) {
+      return done(null, false);
+    }
+    return done(null, user);
+}));
 
 var server = app.listen(PORT, function () {
   console.log('listening on '+PORT);
