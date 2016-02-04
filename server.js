bb6aee193bd6e22eb5a64b4f5a873300efd2eccb
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-03 18:27:55 -1000

    added views

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Modules
 var express = require('express');
+var bodyParser = require('body-parser');
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;  // Want to use Basic Authentication Strategy
 
@@ -10,6 +11,12 @@ var PORT = 3000;
 var app = express();
 var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
 
+app.set('view engine', 'jade');
+app.set('views', 'views');
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
 passport.use(new BasicStrategy(
   function(username, password, done) {
     // Example authentication strategy using
@@ -18,6 +25,16 @@ passport.use(new BasicStrategy(
     }
     return done(null, user);
 }));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/secret',
+  passport.authenticate('basic', { session: false }),
+  function (req, res) {
+    res.json(req.user);
+  });
 
 var server = app.listen(PORT, function () {
   console.log('listening on '+PORT);
